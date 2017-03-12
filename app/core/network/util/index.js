7f9c0c3b2c8f3fa0d067cf6f8a59e83d77510667
obabichev <aq.prots@gmail.com>
2017-03-12 22:11:17 +0300

    amazon url

--- app/core/network/util/index.js
+++ app/core/network/util/index.js
@@ -2,7 +2,9 @@
 
 const _ = require('lodash');
 
-const BASE_URL = 'http://192.168.1.46:3000';
+// const BASE_URL = 'http://192.168.1.46:3000';
+
+const BASE_URL = 'http://ec2-35-157-112-194.eu-central-1.compute.amazonaws.com/olya';
 
 const concatParams = params => _.keys(params).map(key => `${key}=${params[key]}`).join('&');
 
