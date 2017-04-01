80cada850c7ec188405304f25afc78dd937b1755
obabichev <aq.prots@gmail.com>
2017-04-01 15:42:41 +0300

    refresh accesstoken fix

--- app/core/network/auth.js
+++ app/core/network/auth.js
@@ -3,12 +3,26 @@
 import {get, post} from './rest/rest';
 import {genUrl} from './util';
 
+import {validateResponseStatus} from './validator';
+
 export async function fetchRefreshToken(credentionals) {
     let result = await post(genUrl(`/auth`), credentionals);
     return result;
 }
 
 export async function fetchAccessToken(refreshToken) {
-    let result = await post(genUrl(`/refresh`), {refresh_token: refreshToken});
-    return result;
+
+    let result = await fetch(genUrl(`/refresh`), {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({refresh_token: refreshToken})
+    });
+
+    validateResponseStatus(result.status);
+
+    let resultBody = await result.json();
+
+    return resultBody.data;
 }
